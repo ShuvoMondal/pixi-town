0fb1c87e4c2a0154236a4638f9cfe6b948be9548
ShuvoM <d3v.shuvo@gmail.com>
2026-01-18 01:23:07 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@pixi-town/db";
+import * as schema from "@pixi-town/db/schema/auth";
+import { env } from "@pixi-town/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
